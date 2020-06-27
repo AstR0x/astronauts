eda59123bf1e344ee401856ac1625274ff8ab983
Vlad <astr0xy1@gmail.com>
2020-06-27 20:55:01 +0300

    внедрил сортировку в таблице

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-import { splitIntoSubarrays, filterAstronauts } from 'utils';
+import { splitIntoSubarrays, filterAstronauts, orderBy } from 'utils';
 
 import { Form } from 'components/Form';
 import { Filter } from 'components/Filter';
@@ -14,6 +14,8 @@ import styles from './App.module.css';
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [astronauts, setAstronauts] = useState([]);
+  const [sortField, setSortField] = useState('name');
+  const [sortingDirection, setSortingDirection] = useState('asc');
   const [filter, setFilter] = useState('');
   const [currentPage, setCurrentPage] = useState(1);
   const [perPage] = useState(5);
@@ -28,21 +30,37 @@ function App() {
     setFilter(event.target.value);
   };
 
+  const onSort = column => {
+    const direction = sortingDirection === 'asc' ? 'desc' : 'asc';
+
+    setSortField(column);
+    setSortingDirection(direction);
+  };
+
   const onAdd = astronaut => setAstronauts([...astronauts, astronaut]);
   const onDelete = id => setAstronauts(astronauts.filter(astronaut => astronaut.id !== id));
   const onChangePage = pageNumber => setCurrentPage(pageNumber);
 
   const filteredAstronauts = filter ? filterAstronauts(filter, astronauts) : astronauts;
   const subarrays = splitIntoSubarrays(filteredAstronauts, perPage);
-  const displayedAstronauts = subarrays.length ? subarrays[currentPage - 1] : [];
+  const currentSubarray = subarrays[currentPage - 1];
+  const displayedAstronauts = subarrays.length
+    ? orderBy(currentSubarray, sortField, sortingDirection)
+    : [];
   const totalPages = subarrays.length;
 
   return !isLoading ? (
     <div className={styles.app}>
       <Form onAdd={onAdd} />
       <Filter onFilter={onFilter} />
-      <Table onDelete={onDelete} astronauts={displayedAstronauts} />
-      {totalPages && (
+      <Table
+        onSort={onSort}
+        onDelete={onDelete}
+        astronauts={displayedAstronauts}
+        sortField={sortField}
+        sortDirection={sortingDirection}
+      />
+      {totalPages > 1 && (
         <Pagination
           currentPage={currentPage}
           totalPages={totalPages}
--- src/utils/index.js
+++ src/utils/index.js
@@ -25,3 +25,13 @@ export const filterAstronauts = (filter, astronauts) => (
       || lowerCaseDate.includes(lowerCaseFilter)
       || lowerCaseMission.includes(lowerCaseFilter);
   }));
+
+export const orderBy = (array, field, direction) => {
+  const type = typeof array[0][field];
+
+  const sortedArray = type !== 'number'
+    ? array.sort((a, b) => (a[field] > b[field] ? 1 : -1))
+    : array.sort((a, b) => a[field] - b[field]);
+
+  return direction === 'asc' ? sortedArray : sortedArray.reverse();
+};
