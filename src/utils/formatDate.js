44be86a36dfac36d9414a34fea1458cc76ede106
Vlad <astr0xy1@gmail.com>
2020-06-28 17:25:20 +0300

    поправил тесты

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,5 +1,8 @@
-import { MILLISECONDS_IN_SECOND } from '../constants';
+import { MILLISECONDS_IN_SECOND, SECONDS_IN_MINUTE } from '../constants';
 
 const formatter = new Intl.DateTimeFormat('ru', []);
 
-export const formatDate = timestamp => formatter.format(timestamp * MILLISECONDS_IN_SECOND);
+export const formatDate = timestamp => {
+  const date = new Date(timestamp * MILLISECONDS_IN_SECOND);
+  return formatter.format(date.getTime() + date.getTimezoneOffset() * SECONDS_IN_MINUTE * MILLISECONDS_IN_SECOND);
+};
--- src/utils/tests/formatDate.test.js
+++ src/utils/tests/formatDate.test.js
@@ -2,6 +2,6 @@ import { formatDate } from 'utils';
 
 describe('function formatDate should return ', () => {
   it('correct date', () => {
-    expect(formatDate(438814800)).toBe('28.11.1983');
+    expect(formatDate(438814800)).toBe('27.11.1983');
   });
 });
